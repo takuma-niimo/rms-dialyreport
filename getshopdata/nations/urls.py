a70bf488ec901169d5e944518d6865d536871077
Takuma Sato niimo <t.sato@niimo.jp>
2019-08-08 16:54:04 +0900

    fix probrem; static csss are cannot available at /admin/

--- getshopdata/nations/urls.py
+++ getshopdata/nations/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(('dailyreport.urls', 'dailyreport'),)),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
