314cd129f5c75124f74e06012707bf3ed0309813
Takuma Sato niimo <t.sato@niimo.jp>
2019-08-08 14:03:56 +0900

    fix replace.sh

--- replace.sh
+++ replace.sh
@@ -14,9 +14,9 @@ find . -type f -name "*.py" -print0 | xargs -0 sed -i "s/__rmsshopuser__/$rmssho
 find . -type f -name "*.py" -print0 | xargs -0 sed -i "s/__rmsshoppasswd__/$rmsshoppasswd/g"
 find . -type f -name "*.py" -print0 | xargs -0 sed -i "s/__rmsuser__/$rmsuser/g"
 find . -type f -name "*.py" -print0 | xargs -0 sed -i "s/__rmspasswd__/$rmspasswd/g"
-find . -type f -name "*.py" -print0 | xargs -0 sed -i "s/__shopurl__/$shopurl/g"
+find . -type f -name "*.py" -print0 | xargs -0 sed -i "s#__shopurl__#$shopurl#g"
 find . -type f -name "*.py" -print0 | xargs -0 sed -i "s/__nickname__/$nickname/g"
 
 find . -type f -name "*.html" -print0 | xargs -0 sed -i "s/__shopname__/$shopname/g"
-find . -type f -name "*.html" -print0 | xargs -0 sed -i "s/__shopurl__/$shopurl/g"
+find . -type f -name "*.html" -print0 | xargs -0 sed -i "s#__shopurl__#$shopurl#g"
 find . -type f -name "*.html" -print0 | xargs -0 sed -i "s/__nickname__/$nickname/g"
